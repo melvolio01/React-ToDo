e7975fb01f6fe309a993535746798689f8cf9b40
andy hunt <andyhunt200@hotmail.com>
2016-08-20 13:22:20 +0100

    Added search components

--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -18,7 +18,7 @@ var TodoSearch = React.createClass({
         <div>
           <label>
             <input type="checkbox" ref="showCompleted" onChange={this.handleSearch}/>
-            Show completed todos
+            Show completed tasks
           </label>
         </div>
 
