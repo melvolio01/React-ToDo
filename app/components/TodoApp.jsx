668d9d3aabd48cbe13108dc508fa229b4617ade7
andy hunt <andyhunt200@hotmail.com>
2016-08-21 16:45:54 +0100

    New todos rendering, testing added

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
+var uuid = require('node-uuid');
 
 var TodoApp = React.createClass({
   getInitialState: function () {
@@ -10,16 +11,16 @@ var TodoApp = React.createClass({
       searchText: '',
       todos: [
         {
-          id: 1,
+          id: uuid(),
           text: 'Build an app'
         }, {
-          id: 2,
+          id: uuid(),
           text: 'Clean my room'
         }, {
-          id: 3,
+          id: uuid(),
           text: 'Get a new job'
         }, {
-          id: 4,
+          id: uuid(),
           text: 'Write a book'
         }
       ]
@@ -27,7 +28,15 @@ var TodoApp = React.createClass({
   },
 
   handleAddTodo: function (text) {
-    alert('new Todo' + text)
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        {
+          id: uuid(),
+          text: text
+        }
+      ]
+    });
   },
 
   handleSearch: function (showCompleted, searchText) {
